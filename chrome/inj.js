a5930bef89b7277870fad459f847e607c014b536
Wesley Pyburn (TechNobo) <10319195+TcNobo@users.noreply.github.com>
2021-02-08 14:19:18 +0200

    Snackbar

--- chrome/inj.js
+++ chrome/inj.js
@@ -15,45 +15,59 @@ console.log.apply(console, console_info);
 
 window.addEventListener('load', function () {
 	if (window.location.href.includes("inventory")){ // Twitch 
-		// var s = document.createElement('script');
-		// s.src = chrome.extension.getURL('TcNo_RDA_Twitch.js');
-		// s.onload = function() {
-			// this.remove();
-		// };
-		// (document.head || document.documentElement).appendChild(s);
+		addStylesheet();
+		callSnackbar("Loaded");
 		TcNo_RDA_Twitch();
 	}
 	if (window.location.href.includes("facepunch")){ // Twitch 
-		// var s = document.createElement('script');
-		// s.src = chrome.extension.getURL('TcNo_RDA_Twitch.js');
-		// s.onload = function() {
-			// this.remove();
-		// };
-		// (document.head || document.documentElement).appendChild(s);
+		addStylesheet();
+		callSnackbar("Loaded");
 		TcNo_RDA_Fp();
 	}
 });
-
+function addStylesheet(){
+	var s = document.createElement('link');
+	s.type = "text/css";
+	s.rel = "stylesheet";
+	s.href = chrome.extension.getURL('TcNo_snackbar.css');
+	s.onload = function() {
+		this.remove();
+	};
+	(document.head || document.documentElement).appendChild(s);
+	
+	var d = document.createElement('div');
+	d.id = "snackbar";
+	d.innerHTML = "TcNo Rust Drop Assistant Loaded!";
+	(document.body).appendChild(d);
+}
+var fadeout;
+function callSnackbar(text, length = 3000) {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  x.innerHTML = "<h1 class='title'>TcNo Rust Drop Assistant</h1><span>"+text+"</span>";
+  clearTimeout(fadeout);
+  fadeout = setTimeout(()=>{ x.className = "show fadeout"; setTimeout(()=>{x.className = x.className = "";}, 500); }, length);
+}
 
 async function TcNo_RDA_Twitch(){
 	var init_timeout = 0;
 	var claimedItems = []; 
+	if (window.location.href.indexOf("TcNo_update" > -1))callSnackbar("Tab closing after item collection");
 	while (init_timeout < 10){
 		try{
-			[].forEach.call(document.getElementsByClassName("tw-tower")[this.length-1].querySelectorAll('.tw-semibold'), (el)=>{claimedItems.push(escape(el.innerHTML.toLowerCase()))});
-			if (claimedItems.length <= 1){
-				[].forEach.call(document.getElementsByClassName("tw-tower")[this.length].querySelectorAll('.tw-semibold'), (el)=>{claimedItems.push(escape(el.innerHTML.toLowerCase()))});
-				if (claimedItems.length <= 1)throw '';
-			}
-			//console.log("var claimedItems =[\"" + claimedItems.join("\",\"") + "\"]");
-			//copy("var claimedItems =[\"" + claimedItems.join("\",\"") + "\"]");
+			var claimedItemList = document.querySelector('[data-test-selector=drops-list__wrapper]').getElementsByClassName("tw-tower")[0];
+			[].forEach.call(claimedItemList.querySelectorAll('.tw-semibold'), (el)=>{claimedItems.push(escape(el.innerHTML.toLowerCase()))});
+			if (claimedItems.length <= 1)throw '';
 			
-			//(window.chrome) ? chrome.storage.local.set(['claimedItems'], claimedItems.join(",")) : console.log("FIREFOX");
-			await storageProtocol.storage.local.set({claimedItems: claimedItems.join(",")});
+			await storageProtocol.storage.local.set({claimedItems: claimedItems.join(","), lastChecked: new Date().toString()});
 			
 			console.log("Saved info to claimedItems in chrome.storage.local");
+			console.log(claimedItems);
+			callSnackbar("Saved claimed items");
 			
-			init_timeout = 10;
+			init_timeout = 10; // Success
+			
+			if (window.location.href.indexOf("TcNo_update" > -1))window.close();
 		}catch (er){
 			init_timeout++;
 			await new Promise(r => setTimeout(r, 2000));
@@ -73,17 +87,30 @@ function getData_Chrome(sKey) {
   });
 }
 
+toHHMMSS = function (seconds) {
+    var sec_num = parseInt(seconds/1000, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return (hours>0?hours+'h ':"")+(minutes>0?minutes+'m ':"")+(seconds>0?seconds+'s':"");
+}
+
 async function TcNo_RDA_Fp(){
 	var init_timeout = 0;
 	
-	//(window.chrome) ? chrome.storage.local.get(['claimedItems'], (e)=>{claimedItems = e.split(",")}) : console.log("FIREFOX");
 	claimedItems = await getData_Chrome('claimedItems');
+	var lc = await getData_Chrome('lastChecked');
+	console.log("Last checked: " + lc);
 	
-	// console.log("Saved: " + window.localStorage.getItem('claimedItems'))
-	// var claimedItems = browser.localStorage.getItem('claimedItems').split(",");
-	// console.log("Got info from claimedItems in LocalStorage");
-	//console.log("Val: " + claimedItems[0]);
+	var lc_now = new Date().getTime();
+	var lc_d = Date.parse(lc);
+	var timeSince = toHHMMSS(lc_now - lc_d);
 	
+	callSnackbar("Time since last check: " + timeSince+"<br><p style='margin:0;font-size:0.6em'>Last check: " + lc + "</p><a target='_blank' href='https://www.twitch.tv/drops/inventory?TcNo_update'>Update now</a>", 7500);
 	
 	while (init_timeout < 10){
 		try{
@@ -101,14 +128,12 @@ async function TcNo_RDA_Fp(){
 						eIncomplete.push(el.parentElement);
 				}
 			});
-			//[].forEach.call(toRemove, (el)=>{el.remove()}); // Remove elements
-			//[].forEach.call(toRemove, (el)=>{el.setAttribute("style", "background-color:#090E00;outline-offset: -10px;outline:solid 1px #718F41");el.parentElement.appendChild(el)}); // Do something else with elements
 			
 			// Rearrange:
 			var rowCount = 0;
 			[].forEach.call(document.getElementsByClassName('rust-drops__body'), (row)=>{
 				for (let i=0; i < 3; i++){
-					console.log(i);
+					//console.log(i);
 					if (rowCount < 3){
 						if (eIncomplete.length > 0){
 							row.appendChild(eIncomplete[0]);
